997bf784b398fd3f00cec6f2a8d7913c71d7f666
Olifant Qiang <53031172+Alephant6@users.noreply.github.com>
2023-03-27 09:56:20 +0800

    ANTLR C.g4 to cn.olifant.Declaration

---
+++ src/test/java/cn/olifant/C2/MyCVisitor.java
@@ -0,0 +1,9 @@
+package cn.olifant.C2;
+
+public class MyCVisitor extends CBaseVisitor{
+    @Override
+    public Object visitDeclarator(CParser.DeclaratorContext ctx) {
+
+        return super.visitDeclarator(ctx);
+    }
+}
